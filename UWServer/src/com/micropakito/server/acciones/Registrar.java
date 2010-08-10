da2301066cb52b6b1a5f2d914d4979db3c21cc21
micropakito <micropakito@4cc3cb17-523a-1e42-7a16-127342d069f7>
2010-08-10 12:57:39 +0000

    Modificaciones Varias

--- UWServer/src/com/micropakito/server/acciones/Registrar.java
+++ UWServer/src/com/micropakito/server/acciones/Registrar.java
@@ -23,43 +23,71 @@ public class Registrar {
      public Message arranca( Integer id,  String idClase, Class clase, Object obj, ServerHilo serverContext) {
 
         //recibe compartida.Registrar
-
-        Registro reg = (Registro)obj;
-
-        //validaciones
-
-        //existe el usuario
-
-        //mail distinto
-
-        //pass distinto
-
-        // alta en bdd
-
-        Usuarios us = new Usuarios();
-        us.setNick(reg.getNick());
-        us.setNombre(reg.getNombre());
-        us.setPass(reg.getPass());
-        us.setApellidos(reg.getApellidos());
-        us.setMail(reg.getMail());
-        us.setRol(0);
-
-        UsuariosJpaController usjpa = new UsuariosJpaController();
-
-        usjpa.create(us);
-
-
-        Message msg = new Message();
+        Message msgDev = new Message();
+        System.out.println("Va pasando va llegando");
         try {
-            msg.setClase(Class.forName("com.micropakito.compartido.definiciones.RegistroDev"));
-            msg.setDesc("Registrado OK");
-            msg.setID(10001);
-            msg.setObjeto( (Object) new com.micropakito.compartido.definiciones.RegistroDev("Registro molon", "Porque mola mucho", 1)  );
+
+            msgDev.setClase( Class.forName("com.micropakito.compartido.definiciones.RegistroDev" ));
+            msgDev.setDesc("En Registro");
+            msgDev.setID(10001);
+
+            Registro reg = (Registro)obj;
+
+            //validaciones
+            //existe el usuario
+            
+            UsuariosJpaController usrjpa = new UsuariosJpaController();
+
+            Usuarios usExi = (Usuarios) usrjpa.getEntityManager().createNamedQuery("Usuarios.findByNick").setParameter("nick", reg.getNick() );
+            
+            if ( usExi != null ) {
+            
+                msgDev.setObjeto( (Object) new com.micropakito.compartido.definiciones.RegistroDev("El Usuario Ya Exite", "El Usuario ya Existe", 2) );
+                return msgDev;
+            
+            }
+
+            //mail distinto
+            if (! reg.getMail().equals( reg.getMail_rep() )){
+
+                msgDev.setObjeto( (Object) new com.micropakito.compartido.definiciones.RegistroDev("Los Mails no coinciden", "Los Mails No coinciden", 3) );
+                return msgDev;
+            }
+            //pass distinto
+            if (! reg.getPass().equals( reg.getPass_rep() )){
+
+                msgDev.setObjeto( (Object) new com.micropakito.compartido.definiciones.RegistroDev("Los Pass no coinciden", "Los Pass no coinciden", 4) );
+                return msgDev;
+
+            }
+
+            // alta en bdd
+
+            Usuarios us = new Usuarios();
+            us.setNick(reg.getNick());
+            us.setNombre(reg.getNombre());
+            us.setPass(reg.getPass());
+            us.setApellidos(reg.getApellidos());
+            us.setMail(reg.getMail());
+            us.setRol(0);
+
+            UsuariosJpaController usjpa = new UsuariosJpaController();
+
+            usjpa.create(us);
+
+            msgDev.setID(10001);
+            msgDev.setObjeto( (Object) new com.micropakito.compartido.definiciones.RegistroDev("Registro molon", "Porque mola mucho", 1)  );
+            
         } catch (ClassNotFoundException ex) {
             Logger.getLogger(Registrar.class.getName()).log(Level.SEVERE, null, ex);
+            msgDev.setObjeto( (Object) new com.micropakito.compartido.definiciones.RegistroDev("El Usuario Ya Exite", "El Usuario ya Existe", 2) );
+        } catch (Exception exs) {
+
+            System.out.println("Tacaaaaaaaa" + exs.getMessage() );
+
         }
 
-        return msg;
+        return msgDev;
 
      }
 
